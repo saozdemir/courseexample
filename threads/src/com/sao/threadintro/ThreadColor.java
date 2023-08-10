553767b2509adf15b461c2163166d27535262c0f
saozdemir <43U2422a**>
2023-08-10 11:27:37 +0300

    ThreadColor empty commit

--- threads/src/com/sao/threadintro/ThreadColor.java
+++ threads/src/com/sao/threadintro/ThreadColor.java
@@ -10,4 +10,5 @@ public class ThreadColor {
     public static final String ANSI_PURPLE = "\u001B[35m";
     public static final String ANSI_CYAN = "\u001B[36m";
     public static final String ANSI_WHITE = "\u001B[37m";
+
 }
